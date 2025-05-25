0925ee2cdcc74dd2c2413be2869686acfc4f8a07
i-am-vladosina <thisboxforworkismine@gmail.com>
2025-05-25 09:56:12 +0300

    update emitter

--- src/modules/ParticleContainerAdapter.ts
+++ src/modules/ParticleContainerAdapter.ts
@@ -1,17 +1,17 @@
-// import {ParticleContainer} from 'pixi.js';
+import {ParticleContainer} from 'pixi.js';
 
-// import {PixiViewContainer} from 'particle-flux';
+import {ViewContainer} from 'particle-flux';
 
-// import {ParticleAdapter} from './ParticleAdapter';
+import {ParticleAdapter} from './ParticleAdapter';
 
-// export class ParticleContainerAdapter implements PixiViewContainer<ParticleAdapter> {
-//   constructor(private readonly container: ParticleContainer) {}
+export class ParticleContainerAdapter implements ViewContainer<ParticleAdapter> {
+  constructor(private readonly container: ParticleContainer) {}
 
-//   addChild(children: ParticleAdapter): void {
-//     this.container.addParticle(children.particle);
-//   }
+  addChild(children: ParticleAdapter): void {
+    this.container.addParticle(children.particle);
+  }
 
-//   removeChild(children: ParticleAdapter): void {
-//     this.container.removeParticle(children.particle);
-//   }
-// }
+  removeChild(children: ParticleAdapter): void {
+    this.container.removeParticle(children.particle);
+  }
+}
